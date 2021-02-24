b0046924acde82970a77160df999d70d1bff1ce9
jcvara <jcvp80@gmail.com>
2021-02-24 16:11:11 +0100

    Added form

--- server.js
+++ server.js
@@ -1,9 +1,32 @@
+// Module Imports
 const express = require('express');
 const app = express();
+const path = require(`path`);
+const bodyParser = require('body-parser');
 
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+// GET Handlers
 app.get('/', (req, res) => {
   res.send('Hello from Ciberseguridad!');
 });
+
+app.get('/submit', (req, res) => {
+  res.sendFile(path.join(__dirname, '/views/form.html'));
+});
+
+
+// POST Handlers
+app.post('/submit', (req, res) => {
+  console.log({
+    name: req.body.name,
+    message: req.body.message
+  });
+  res.send('Thanks for your message!');
+});
+
 
 // Listen to the App Engine-specified port, or 8080 otherwise
 const PORT = process.env.PORT || 8080;
